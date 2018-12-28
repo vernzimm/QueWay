01562baf66daf2ab9fb008c5e38c1d7ae43282ab
vernzimm <vern.zimm@gmail.com>
2018-12-28 18:19:03 -0500

    Set up a settings file loader (in MiscFunc) for both the QueWay.ini, and grid file(s). The Offline script will be able to load MiscFunc and QueWay.ini as well. That part works, but something is broken with the root_loop() thing, and/or TPEx.

--- MiscFunc.py
+++ MiscFunc.py
@@ -1,1 +1,39 @@
 from __main__ import *
+
+
+def load_settings(watfile,watvars) :
+    
+    b = {}
+    c = []
+    d = []
+    e = watfile.split(sep = '\\')[-1]
+    
+    try :
+        file = open(watfile,'r')
+    except :
+        write_debug('Was not able to open the settings file \"' + e + '\"!\n')
+        return(b,c,d)
+    
+    a = 1
+    for i in file.readlines() :
+        i = i.strip()
+        if len(i) != 0 and i[0] != '#' :
+
+            write_debug(e + ' entry#' + str(a) + ': ' + i + '\n')
+            a += 1
+
+            j = i.split(sep = '=')
+            for k in range(len(j)) :
+                j[k] = j[k].strip()
+
+            for k in watvars :
+                if j[0] == k :
+                    if k == 'spcname' :
+                        c = c + [j[1]]
+                    elif k == 'gridfile' :
+                        d = d + [j[1]]
+                    else :
+                        b[k] = j[1]
+    
+    file.close()
+    return(b,c,d)
