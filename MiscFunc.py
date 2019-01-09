038e88dc4d54992818da1d17a069044679583d7c
vernzimm <vern.zimm@gmail.com>
2019-01-08 22:29:11 -0500

    Added a function to check for file or folder path, and make if needed. Also small function to swap any "\\" for "/" in paths. Some bug in the que section relating to the quenext variable. I think if some items are deleted from que, but now quenext is higher than # items, it does... something. Added random program run result to offline

--- MiscFunc.py
+++ MiscFunc.py
@@ -1,45 +1,128 @@
 from __main__ import *
 
+#This function loads a given file and parses variables QueWay.ini and Grid.txt stored in ./
+#ToDo: Make this generic...
+#Pass[0]: file path including extension
+#Pass[1]: list of variable names that will match the incoming variables
+#Return[0]: dictionary matched variable pairs
+#Return[1]: special case "spcname"
+#Return[2]: special case "gridname"
 
 def load_settings(watfile,watvars) :
-    
-    b = {}
-    c = []
-    d = []
-    e = watfile.split(sep = '\\')[-1]
-    
-    try :
-        file = open(watfile,'r')
-    except :
-        write_debug('Was not able to open the settings file \"' + e + '\"!\n')
-        return(b,c,d)
-    
-    a = 1
-    for i in file.readlines() :
-        i = i.strip()
-        if len(i) != 0 and i[0] != '#' :
+	
+	test_dir(watfile,False)
+	watfile = clean_win_path(watfile)
+	e = watfile.rsplit('/',1)[1]
+	
+	b = {}
+	c = []
+	d = []
+	
+	try :
+		file = open(watfile,'r')
+	except :
+		write_debug('Was not able to open the settings file \"' + e + '\"!\n')
+		return(b,c,d)
+	
+	a = 1
+	for i in file.readlines() :
+		i = i.strip()
+		if len(i) != 0 and i[0] != '#' :
 
-            write_debug(e + ' entry#' + str(a) + ': ' + i + '\n')
-            a += 1
+			write_debug(e + ' entry#' + str(a) + ': ' + i + '\n')
+			a += 1
 
-            j = i.split(sep = '=')
-            for k in range(len(j)) :
-                j[k] = j[k].strip()
+			j = i.split(sep = '=')
+			for k in range(len(j)) :
+				j[k] = j[k].strip()
 
-            for k in watvars :
-                if j[0] == k :
-                    if k == 'spcname' :
-                        c = c + [j[1]]
-                    elif k == 'gridfile' :
-                        d = d + [j[1]]
-                    elif k == 'retrycnt' :
-                        b[k] = int(j[1])
-                    elif j[1] == 'True' :
-                        b[k] = True
-                    elif j[1] == 'False' :
-                        b[k] = False
-                    else :
-                        b[k] = j[1]
-    
-    file.close()
-    return(b,c,d)
+			for k in watvars :
+				if j[0] == k :
+					if k == 'spcname' :
+						c = c + [j[1]]
+					elif k == 'gridfile' :
+						d = d + [j[1]]
+					elif k == 'retrycnt' :
+						b[k] = int(j[1])
+					elif j[1] == 'True' :
+						b[k] = True
+					elif j[1] == 'False' :
+						b[k] = False
+					else :
+						b[k] = j[1]
+	
+	file.close()
+	return(b,c,d)
+
+
+#Check a directory or file exists, and make it if requested (make path up to file).
+#Changes "\\" to "/" so using "./" or ".\\" is OK. If want to make a folder with "." in name (i.e. ./Prog V1.0 Folder)...
+#Enclose with "/" after (i.e. ./Prog V1.0 Folder/)
+#Pass[0]: directory or file path (differentiates by existence of file.ext at end of path)
+#Pass[1]: True or False to make the file or path if it doesn't exist
+#Return[0]: True or False exists (assumed was made if didn't exist etc)
+
+def test_dir(watpath,domake) :
+	
+	b = clean_win_path(watpath)
+	
+	a = b.rsplit('/',1)[1]
+	a = a.split(sep = '.')
+	
+	dopath = True
+	if len(a) > 1 and a[1] != '' :
+		dopath = False
+
+	a = os.path.exists(b)
+		
+	if a :
+		write_debug('\"' + b + '\" already exists.\n')
+		have = True
+	elif domake :
+		try :
+			if dopath :
+				write_debug('trying to make the path \"' + b + '\"\n')
+				c = ''
+				for i in b.split('/') :
+					c = c + i + '/'
+					if not test_dir(c,False) :
+						d = c.rsplit('/',1)[0]
+						e = test_dir(d,False)
+						if e :
+							write_debug('A file \"' + d + '\" exists and a folder \"' + c + '\" can not be made!\n')
+							raise
+						else :
+							os.makedirs(c)
+			else :
+				a = b.rsplit('/', 1)[0] + '/'
+				a = test_dir(a,True)
+				if a :
+					write_debug('trying to make the file \"' + b + '\"\n')
+					file = open(b,'w')
+					file.close
+		except :
+			write_debug('Failed to make \"' + b + '\"!\n')
+			have = False
+		else :
+			write_debug('\"' + b + '\" did not exist, but was made successfully.\n')
+			have = True
+	else :
+		write_debug('\"' + b + '\" does not exist and did not try to make.\n')
+		have = False
+
+	return(have)
+	
+	
+#Change backslash to forward for path strings
+#Pass[0]: path
+#Return[0]: cleaned path
+def clean_win_path(inpath) :
+
+	b = ''
+	for i in inpath :
+		if i == '\\' :
+			b = b + '/'
+		else :
+			b = b + i
+		
+	return(b)
--- RMFunc.py
+++ RMFunc.py
@@ -5,24 +5,25 @@ from __main__ import *
 #Returns [0]: True or False file was found
 
 def have_file(file) :
-    
-    b = 0
-    d = 0
-    while b <= 500 :
-        get = os.path.isfile(file)
-        if get == True :
-            break
-        time.sleep(0.01)
-        d += 0.01
-        b += 1
-    d = str(round(d,2))
-    
-    if get == True :
-        write_debug('have file: ' + file + ' after ' + d + ' seconds.\n')
-    else :   
-        write_debug('do not have file: ' + file + ' after ' + d + ' seconds.\n')
-        
-    return(get)
+
+	file = clean_win_path(file)
+	b = 0
+	d = 0
+	while b <= 500 :
+		get = os.path.isfile(file)
+		if get == True :
+			break
+		time.sleep(0.01)
+		d += 0.01
+		b += 1
+	d = str(round(d,2))
+	
+	if get == True :
+		write_debug('have file: ' + file + ' after ' + d + ' seconds.\n')
+	else :   
+		write_debug('do not have file: ' + file + ' after ' + d + ' seconds.\n')
+		
+	return(get)
 
 
 #Delete a file
@@ -30,31 +31,32 @@ def have_file(file) :
 #Return [0]: True or False did delete file (under 3 seconds)
 
 def delete_file(file) :
-    
-    did = not os.path.isfile(file)
-    
-    if did :
-        write_debug('did not find: ' + file + ' to delete.\n')
-        
-        return(did)
 
-    a = 0
-    while os.path.isfile(file) and a <= 1 :
-        try :
-            os.remove(file)
-        except OSError :
-            time.sleep(0.01)
-        a += 0.01
-        
-    a = str(round(a,2))
-    
-    did = not os.path.isfile(file)
-    if did :
-        write_debug('did delete: ' + file + ' after ' + a + ' seconds.\n')
-    else :
-        write_debug('could not delete: ' + file + ' after ' + a + ' seconds and file exists = ' + str(os.path.isfile(file)) + '.\n')
+	file = clean_win_path(file)
+	did = not os.path.isfile(file)
+	
+	if did :
+		write_debug('did not find: ' + file + ' to delete.\n')
+		
+		return(did)
 
-    return(did)
+	a = 0
+	while os.path.isfile(file) and a <= 1 :
+		try :
+			os.remove(file)
+		except OSError :
+			time.sleep(0.01)
+		a += 0.01
+		
+	a = str(round(a,2))
+	
+	did = not os.path.isfile(file)
+	if did :
+		write_debug('did delete: ' + file + ' after ' + a + ' seconds.\n')
+	else :
+		write_debug('could not delete: ' + file + ' after ' + a + ' seconds and file exists = ' + str(os.path.isfile(file)) + '.\n')
+
+	return(did)
 
 
 #Try to open file in append mode and read the first 8 characters.
@@ -62,28 +64,29 @@ def delete_file(file) :
 #Return[0]: True or False file was locked
 
 def is_locked(file):
-    
-    locked = True
-    d = 0
-    while locked and d < 3 :
-        try:
-            a = open(file, 'a', 8)
-            if a :
-                locked = False
-                a.close()
-        except IOError:
-            locked = True
-            time.sleep(0.01)
-        d += 0.01
+	
+	file = clean_win_path(file)
+	locked = True
+	d = 0
+	while locked and d < 3 :
+		try:
+			a = open(file, 'a', 8)
+			if a :
+				locked = False
+				a.close()
+		except IOError:
+			locked = True
+			time.sleep(0.01)
+		d += 0.01
 
-    d = str(round(d,2))
-    
-    if locked :
-        write_debug(file + ' is still locked after ' + d + ' seconds.\n')
-    else :
-        write_debug(file + ' is not locked after ' + d + ' seconds.\n')
+	d = str(round(d,2))
+	
+	if locked :
+		write_debug(file + ' is still locked after ' + d + ' seconds.\n')
+	else :
+		write_debug(file + ' is not locked after ' + d + ' seconds.\n')
 
-    return (locked)
+	return (locked)
 
 
 #Read in a file and delete after if needed
@@ -92,35 +95,36 @@ def is_locked(file):
 #Return [0]: cleaned file data
 
 def read_file(file,delete) :
-    
-    data = []
-    
-    got = have_file(file)
-    if got == False :
-        return(data)
-    
-    lock = is_locked(file)
-    if lock :
-        return(data)
-    
-    a = 0
-    try :
-        b = open(file,'r')
-        for i in b :
-            e = i.rstrip()
-            data = data + [e]
-            a += 1
-        b.close()
-        write_debug('Did read ' + str(a) + ' lines from: ' + file + '.\n')
-    except OSError :
-        write_debug('Did not read ' + file + ' and exists = ' + str(os.path.isfile(file)) + '.\n')
-    
-    if delete :
-        delete_file(file)
-    else :
-        write_debug('Did not attempt to delete ' + file + ' and exists = ' + str(os.path.isfile(file)) + '.\n')
-    
-    return(data)
+	
+	file = clean_win_path(file)
+	data = []
+	
+	got = have_file(file)
+	if got == False :
+		return(data)
+	
+	lock = is_locked(file)
+	if lock :
+		return(data)
+	
+	a = 0
+	try :
+		b = open(file,'r')
+		for i in b :
+			e = i.rstrip()
+			data = data + [e]
+			a += 1
+		b.close()
+		write_debug('Did read ' + str(a) + ' lines from: ' + file + '.\n')
+	except OSError :
+		write_debug('Did not read ' + file + ' and exists = ' + str(os.path.isfile(file)) + '.\n')
+	
+	if delete :
+		delete_file(file)
+	else :
+		write_debug('Did not attempt to delete ' + file + ' and exists = ' + str(os.path.isfile(file)) + '.\n')
+	
+	return(data)
 
 
 #Wait loop to read a file using read_file. Args are passed thru directly.
@@ -129,16 +133,16 @@ def read_file(file,delete) :
 #Return [0]: returned file data
 
 def wait_read(file,delete) :
-    
-    result = []
-    while result == [] :
-        print('waiting for read msg')
-        result = read_file(file,delete)
-        time.sleep(1)
+	
+	file = clean_win_path(file)
+	result = []
+	while result == [] :
+		result = read_file(file,delete)
+		time.sleep(1)
 
-    rootque.put('kill')
-    return(result)
-        
+	rootque.put('kill')
+	return(result)
+	
 
 #Do comm1 one style command (rask->rans)
 #Pass[0]: Directory to use to communicate
@@ -146,47 +150,47 @@ def wait_read(file,delete) :
 #Return[0]: RM answer data
 
 def comm1(commloc,command) :
-    
-    retries = retrycnt
+	
+	retries = retrycnt
+	commloc = clean_win_path(commloc)
+	rask = commloc + 'REMOTE.ASK'
+	rans = commloc + 'REMOTE.ANS'
+	
+	command = command + '\n'
+	
+	while retries > 0 :
+		delete_file(rans)
 
-    rask = commloc + 'REMOTE.ASK'
-    rans = commloc + 'REMOTE.ANS'
-    
-    command = command + '\n'
-    
-    while retries > 0 :
-        delete_file(rans)
+		cmd = open(rask,'w')
+		cmd.write(command)
+		cmd.close()
 
-        cmd = open(rask,'w')
-        cmd.write(command)
-        cmd.close()
+		cmdis = command.splitlines()[0]
 
-        cmdis = command.splitlines()[0]
+		write_debug('Wrote command: ' + cmdis + ' to ' + rask + '.\n')
 
-        write_debug('Wrote command: ' + cmdis + ' to ' + rask + '.\n')
+		line = 1
+		for i in command.splitlines() :
+			write_debug('rask line ' + str(line) + ': ' + i + '\n')
+			line += 1
 
-        line = 1
-        for i in command.splitlines() :
-            write_debug('rask line ' + str(line) + ': ' + i + '\n')
-            line += 1
+		reply = read_file(rans,True)
 
-        reply = read_file(rans,True)
+		if reply != [] :
+			a = retrycnt - (retries - 1)
+			write_debug('Completed command: ' + cmdis + ' after ' + str(a) + ' tries.\n')
+			line = 1
+			for i in reply :
+				write_debug('rans line ' + str(line) + ': ' + i + '\n')
+				line += 1
+			retries = 0
+		else :
+			write_debug('Did not complete command: ' + cmdis + ' successfully.\n')
+			retries -= 1
+			write_debug('Retrying ' + str(retries) + ' more times!\n')
 
-        if reply != [] :
-            a = retrycnt - (retries - 1)
-            write_debug('Completed command: ' + cmdis + ' after ' + str(a) + ' tries.\n')
-            line = 1
-            for i in reply :
-                write_debug('rans line ' + str(line) + ': ' + i + '\n')
-                line += 1
-            retries = 0
-        else :
-            write_debug('Did not complete command: ' + cmdis + ' successfully.\n')
-            retries -= 1
-            write_debug('Retrying ' + str(retries) + ' more times!\n')
-
-    rootque.put('kill')
-    return(reply)
+	rootque.put('kill')
+	return(reply)
 
 
 #Write a file (misc, no specific format). Writes a tmp file first to complete, then copies.
@@ -195,22 +199,23 @@ def comm1(commloc,command) :
 #Pass[2]: optional : True or False to delete the file if it already exists
 
 def write_file(what,where,delete=False) :
-    
-    if delete :
-        delete_file(where)
-    
-    tmp = where.rsplit(sep = '\\', maxsplit = 1)
-    tmp = tmp[0] + '\\temp' + tmp[1]
-    
-    line = 1
-    cmd = open(tmp,'w')
-    for i in what :
-        cmd.write(str(i) + '\n')
-        write_debug('rans line ' + str(line) + ': ' + i + '\n')
-        line += 1
-    cmd.close()
-    
-    copy(tmp,where)
-    
-    write_debug('Wrote file: ' + where + '.\n')
-    return()
+	
+	where = clean_win_path(where)
+	if delete :
+		delete_file(where)
+	
+	tmp = where.rsplit('/',1)
+	tmp = tmp[0] + '/temp' + tmp[1]
+	
+	line = 1
+	cmd = open(tmp,'w')
+	for i in what :
+		cmd.write(str(i) + '\n')
+		write_debug('rans line ' + str(line) + ': ' + i + '\n')
+		line += 1
+	cmd.close()
+	
+	copy(tmp,where)
+	
+	write_debug('Wrote file: ' + where + '.\n')
+	return()
